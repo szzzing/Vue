ced0a4e4373f402727addde2a4b9828530e11d99
DESKTOP-HE0SPDG\ta9 <xxszzzing@gmail.com>
2023-07-21 14:26:37 +0900

    update: 실습 - 모달 이전

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>TODO it!😃</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    hl {
+        color: #2f3b52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" placeholder="Type what you have to do">
+    <span class="addContainer" v-on:click="addTodo">
+        <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            newTodoItem: ''
+        }
+    },
+    methods: {
+        addTodo: function() {
+            if(this.newTodoItem!=='') {
+                var value = this.newTodoItem && this.newTodoItem.trim();
+                this.$emit('addTodo', value);
+                this.clearInput();
+            }
+        },
+        clearInput: function() {
+            this.newTodoItem = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    input {
+        width: calc(100% - 80px);
+    }
+    input:focus {
+        outline: none;
+    }
+    .inputBox {
+        background: white;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    .inputBox input {
+        border-style: none;
+        font-size: 0.9rem;
+    }
+    .addContainer {
+        float: right;
+        background: linear-gradient(to right, #6478FB, #8763FB);
+        display: inline-block;
+        width: 3rem;
+        border-radius: 0 5px 5px 0;
+    }
+    .addBtn {
+        color: white;
+        vertical-align: middle;
+    }
+</style>
