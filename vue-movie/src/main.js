df3ce2a7d51a6ba406383831144886b95f8c9463
szzzing <xxszzzing@gmail.com>
2023-07-31 17:11:19 +0900

    build: git 레퍼지토리 분할

--- vue-movie/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { store } from './store/store'
-import { router } from './router/index'
-
-createApp(App)
-    .use(router)
-    .use(store)
-    .mount('#app')
--- vue-movie/src/store/state.js
+++
@@ -1,8 +0,0 @@
-export const state = {
-    headerText: 'Reminiscene',
-    query: '',
-    details: {},
-    searchList: {},
-    page: 1,
-    maxPage: 0
-}
